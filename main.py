31829d9e0a57bea92b0a049a20f59bedf87711f4
Nunyuki <cdaielodie@gmail.com>
2025-11-12 12:48:49 +0100

    Ajout contact v3

--- main.py
+++ main.py
@@ -19,25 +19,23 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-# Conteneur pour le header avec le lien à gauche
-header = st.container()
-with header:
-    col1, col2 = st.columns([1, 5])  # 1/6 pour le lien, 5/6 pour le reste
-    with col1:
-        st.markdown(
-            '<a class="contact-link" href="https://nunyuki-portfolio-dev.streamlit.app/pages/00_Contact">📞 Me contacter</a>',
-            unsafe_allow_html=True
-        )
-    with col2:
-        st.title("Bonjour, je suis Elodie DAI")
-        st.subheader("Diplômée de la formation Master MIAGE à l'Université Paris Dauphine - PSL")
-
-# Boutons projets / CV
-col1, col2 = st.columns(2)
+# --- Header avec lien Me contacter ---
+col1, col2 = st.columns([1, 5])  # 1/6 pour le lien, 5/6 pour le titre
 with col1:
+    st.markdown(
+        '<a class="contact-link" href="https://nunyuki-portfolio-dev.streamlit.app/pages/00_Contact" target="_blank">📞 Me contacter</a>',
+        unsafe_allow_html=True
+    )
+with col2:
+    st.title("Bonjour, je suis Elodie DAI")
+    st.subheader("Diplômée de la formation Master MIAGE à l'Université Paris Dauphine - PSL")
+
+# --- Boutons ---
+col3, col4 = st.columns(2)
+with col3:
     if st.button("Voir mes projets"):
         st.write("➡️ Ici tu peux ajouter un lien ou faire défiler vers tes projets.")
 
-with col2:
-    if st.button("Télécharger mon CV"):
+with col4:
+    if st.button("Télécharger mon CV cc" ):
         st.write("➡️ Ici tu peux mettre un lien pour télécharger ton CV PDF.")
