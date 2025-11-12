53c2e0531f12657a465ebfb78dafaf2bdcc1d4a5
Nunyuki <cdaielodie@gmail.com>
2025-11-12 12:56:36 +0100

    Ajout requirement

--- pages/00_Contact.py
+++ pages/00_Contact.py
@@ -6,3 +6,7 @@ st.write("""
 🚀 Des questions, un projet ou une opportunité professionnelle ?  
 Je serais ravi d’en discuter avec vous ! N'hésitez pas à me contacter.
 """)
+
+st.write("📧 Email : daichenelodie@gmail.com")
+st.write("🔗 [LinkedIn](https://www.linkedin.com/in/dai-elodie/)")
+st.write("🐙 [GitHub](https://github.com/Nunyuki)")
